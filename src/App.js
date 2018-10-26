6876b8accdd45d2897a3f7bb052527bff98e11d4
David <dadams03@gmail.com>
2018-10-25 20:38:23 -0700

    Working click me button

--- src/App.js
+++ src/App.js
@@ -3,17 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { doTest } from './redux/actions';
+import Header from './Header';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'black',
+      banner: 'hello',
+      isOpen: false,
+    };
+    this.buttonHandler = this.buttonHandler.bind(this);
+    this.textHandler = this.textHandler.bind(this);
+  }
+
+  buttonHandler() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  textHandler(e) {
+    this.setState({
+      banner: e.target.value,
+    })
+  }
+
   render() {
+    let myVariable = <h2>David</h2>;
+    let myBanner;
+    if (this.state.isOpen) {
+      myBanner = <Header banner={this.state.banner}/>;
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {myVariable}
           </p>
-          <h2>413 Starter Kit </h2>
+          {myBanner}
+          
+          <input value={this.state.banner} onChange={this.textHandler}/>
+          <button onClick={this.buttonHandler} >Click Me</button>
         </header>
       </div>
     );
